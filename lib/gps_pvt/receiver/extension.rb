b5bb994296c03f75c811f6c77b0da514bdc02c54
fenrir <fenrir.naru@gmail.com>
2022-11-29 23:41:43 +0900

    Make multiple online_ephemeris options available

--- lib/gps_pvt/receiver/extension.rb
+++ lib/gps_pvt/receiver/extension.rb
@@ -29,19 +29,24 @@ class Receiver
     return (eph.valid?(t) ? eph : nil)
   end
 
-  def attach_online_ephemeris(uri_template = nil)
-    if (!uri_template) || (uri_template =~ /^\s*$/) then
-      uri_template = "ftp://gssc.esa.int/gnss/data/daily/%Y/brdc/BRDC00IGS_R_%Y%j0000_01D_MN.rnx.gz"
-    end
+  def attach_online_ephemeris(uri_template = [nil])
+    uri_template = uri_template.collect{|v|
+      if (!v) || (v =~ /^\s*$/) then
+        "ftp://gssc.esa.int/gnss/data/daily/%Y/brdc/BRDC00IGS_R_%Y%j0000_01D_MN.rnx.gz"
+      else
+        v
+      end
+    }.uniq
     loader = proc{|t_meas|
       utc = Time::utc(*t_meas.c_tm)
-      uri = URI::parse(utc.strftime(uri_template))
-      begin
-        self.parse_rinex_nav(uri)
-      rescue Net::FTPError, Net::HTTPExceptions => e
-        $stderr.puts "Skip to read due to %s (%s)"%[e.inspect.gsub(/[\r\n]/, ' '), uri]
-      end
-      uri
+      uri_template.each{|v|
+        uri = URI::parse(utc.strftime(v))
+        begin
+          self.parse_rinex_nav(uri)
+        rescue Net::FTPError, Net::HTTPExceptions => e
+          $stderr.puts "Skip to read due to %s (%s)"%[e.inspect.gsub(/[\r\n]/, ' '), uri]
+        end
+      }
     }
     run_orig = self.method(:run)
     eph_list = {}
