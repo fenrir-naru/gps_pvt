d5e3264f367270290685773e8dd906634bddead9
fenrir <fenrir.naru@gmail.com>
2022-12-16 23:47:14 +0900

    Make ntrip test flexible with environment to change caster

--- spec/gps_pvt/ntrip_spec.rb
+++ spec/gps_pvt/ntrip_spec.rb
@@ -6,13 +6,16 @@ require 'timeout'
 require 'gps_pvt/ntrip'
 
 RSpec::describe GPS_PVT::Ntrip do
-  let(:caster){['rtk2go.com', 2101]}
-  let(:auth){['test@example.com', 'none']}
+  before{skip "Ntrip caster is not specified by ENV['NTRIP_CASTER']" unless ENV['NTRIP_CASTER']}
+  let(:caster){ENV['NTRIP_CASTER'].split(':')} # 'rtk2go.com:2101'
+  let(:auth){(ENV['NTRIP_AUTH'] || '').split(':')} # 'test@example.com:none'
   let(:header_options){
     {'Ntrip-Version' => [nil, 'Ntrip/1.0', 'Ntrip/2.0']}
   }
   it "can acquire data" do
-    opt = header_options.merge({:basic_auth => [auth]}).collect{|k, v|
+    opt = header_options.merge({
+      :basic_auth => [auth.empty? ? nil : auth],
+    }).collect{|k, v|
       v.kind_of?(Array) ? v.collect{|v2| [k, v2]} : [[k, v]]
     }
     opt[0].product(*opt[1..-1]).each{|header|
@@ -55,9 +58,10 @@ RSpec::describe GPS_PVT::Ntrip do
         ubx_pt = pt_list.select{|k, v|
           v[:format] =~ /ubx|u-?blox/i
         }
-        next ubx_pt.to_a.first[0] unless ubx_pt.empty?
+        next ubx_pt.keys.first unless ubx_pt.empty?
+        pt_list.keys.first
       }
-      next if mnt_pt.empty?
+      next unless mnt_pt
       
       top_level = "#{host}#{":#{port}" if port}"
       uri_str = "ntrip://#{top_level}/#{mnt_pt}"
@@ -65,6 +69,7 @@ RSpec::describe GPS_PVT::Ntrip do
       auth_str = "#{URI::encode_www_form_component(uname)}:#{pass}@" if uname
       uri_str_with_auth = "ntrip://#{auth_str}#{top_level}/#{mnt_pt}"
       uri_with_auth = URI::parse(uri_str)
+      auth_hash = uname ? {:basic_auth => auth} : {}
       
       expect(uri).to be_a(URI::Ntrip)
       expect(uri_with_auth).to be_a(URI::Ntrip)
@@ -76,11 +81,11 @@ RSpec::describe GPS_PVT::Ntrip do
       }
       
       params_list = [
-        [uri, :open, {:basic_auth => auth}],
+        [uri, :open, auth_hash],
         [uri_with_auth, :open],
-        [:open, uri, {:basic_auth => auth}],
+        [:open, uri, auth_hash],
         [:open, uri_with_auth],
-        [:open, uri_str, {:basic_auth => auth}],
+        [:open, uri_str, auth_hash],
         [:open, uri_str_with_auth],
       ].collect{|params|
         options.collect{|opt|
@@ -88,7 +93,9 @@ RSpec::describe GPS_PVT::Ntrip do
           params2 << opt.merge((params2[-1].kind_of?(Hash) ? params2.pop : {}))
           params2
         }
-      }.flatten(1).shuffle.values_at(0) # Just one case to reduce the access
+      }.flatten(1).uniq
+      
+      #params_list = params_list.shuffle.values_at(0) # Just one case to reduce the access
       
       params_list.each{|params|
         $stderr.puts "Connecting #{params} ..."
