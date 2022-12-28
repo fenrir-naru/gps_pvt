6548b1749585c9798bcbfffa8a1b49140dc696e7
fenrir <fenrir.naru@gmail.com>
2022-12-28 09:42:30 +0900

    Add body decoder of message type 1077 to RTCM3

--- lib/gps_pvt/rtcm3.rb
+++ lib/gps_pvt/rtcm3.rb
@@ -12,69 +12,118 @@ class RTCM3
     GPS_PVT::Util::CRC24Q::checksum(packet[range])
   end
   module Packet
-    def decode(bits_list, offset = 24)
-      Util::BitOp::extract(self, bits_list, offset)
+    def decode(bits_list, offset = nil)
+      Util::BitOp::extract(self, bits_list, offset || 24)
     end
     def message_number
       decode([12]).first
     end
     DataFrame = proc{
-      unum = proc{|n, sf|
-        [n, proc{|v| sf.kind_of?(Rational) ? (v * sf).to_f : (v * sf)}]
+      unum_gen = proc{|n, sf|
+        sf ||= 1
+        [n, sf.kind_of?(Rational) ? proc{|v| (sf * v).to_f} : proc{|v| sf * v}]
       }
-      num = proc{|n, sf|
-        [n, proc{|v| 
-          v -= (1 << n) if v >= (1 << (n - 1))
-          v *= sf if sf
-          v = v.to_f if sf.kind_of?(Rational)
-          v
-        }]
+      num_gen = proc{|n, sf|
+        lim = 1 << (n - 1)
+        lim2 = lim << 1
+        sf ||= 1
+        [n, sf.kind_of?(Rational) ?
+            proc{|v| v -= lim2 if v >= lim; (sf * v).to_f} :
+            proc{|v| v -= lim2 if v >= lim; sf * v}]
       }
-      df = {
-        1 => [1],
+      idx_list_gen = proc{|n, start|
+        start ||= 0
+        idx_list = (start...(start+n)).to_a.reverse
+        [n, proc{|v| idx_list.inject([]){|res, idx|
+          res.unshift(idx) if (v & 0x1) > 0
+          break res unless (v >>= 1) > 0
+          res
+        } }]
+      }
+      df = { # {df_num => [bits, post_process] or generator_proc, ...}
+        1 => proc{|n| [n]},
         2 => [12],
         3 => [12],
         21 => [6],
         22 => [1],
         23 => [1],
         24 => [1],
-        25 => num.call(38, Rational(1, 10000)),
+        25 => num_gen.call(38, Rational(1, 10000)),
         141 => [1],
         142 => [1],
         364 => [2],
         393 => [1],
-        394 => [64],
-        395 => [32],
-        404 => num.call(15, Rational(1, 10000)),
-        405 => num.call(20, Rational(1, 1 << 29)),
-        406 => num.call(24, Rational(1, 1 << 31)),
+        394 => idx_list_gen.call(64, 1),
+        395 => idx_list_gen.call(32, 1),
+        396 => proc{|df394, df395|
+          x_list = df395.product(df394).collect{|sig_prn| sig_prn.reverse}
+          idx_list = idx_list_gen.call(x_list.size)[1]
+          [x_list.size, proc{|v| x_list.values_at(*idx_list.call(v))}]
+        },
+        397 => [8],
+        398 => unum_gen.call(10, Rational(1, 1 << 10)),
+        399 => num_gen.call(14),
+        404 => num_gen.call(15, Rational(1, 10000)),
+        405 => num_gen.call(20, Rational(1, 1 << 29)),
+        406 => num_gen.call(24, Rational(1, 1 << 31)),
         407 => [10],
-        408 => unum.call(10, Rational(1, 1 << 4)),
+        408 => unum_gen.call(10, Rational(1, 1 << 4)),
         409 => [3],
         411 => [2],
         412 => [2],
         417 => [1],
-        418 => [1],
+        418 => [3],
         420 => [1],
-        :uint30 => [30],
+        :uint => proc{|n| [n]},
       }
       df[27] = df[26] = df[25]
+      df.define_singleton_method(:generate_prop){|idx_list|
+        hash = Hash[*([:bits, :op].collect.with_index{|k, i|
+          [k, idx_list.collect{|idx, *args|
+            case prop = self[idx]
+            when Proc; prop = prop.call(*args)
+            end
+            prop[i]
+          }]
+        }.flatten(1))].merge({:df => idx_list})
+        hash[:bits_total] = hash[:bits].inject{|a, b| a + b}
+        hash
+      }
       df
     }.call
     MessageType = Hash[*({
-      1005 => [2, 3, 21, 22, 23, 24, 141, 25, 142, 1, 26, 364, 27],
-      1077 => [2, 3, :uint30, 393, 409, 1, 411, 412, 417, 418, 394, 395], # 169 bits @see Table 3.5-78
-    }.collect{|mt, df_list|
-      [mt, Hash[*([:bits, :op].collect.with_index{|k, i|
-        [k, df_list.collect{|df| DataFrame[df][i]}]
-      }.flatten(1))].merge({:df => df_list})]
-    }.flatten(1))]
+      1005 => [2, 3, 21, 22, 23, 24, 141, 25, 142, [1, 1], 26, 364, 27],
+      1077 => [2, 3, [:uint, 30], 393, 409, [1, 7], 411, 412, 417, 418, 394, 395], # 169 bits @see Table 3.5-78
+    }.collect{|mt, df_list| [mt, DataFrame.generate_prop(df_list)]}.flatten(1))]
     def parse
-      return nil unless (mt = MessageType[message_number])
+      msg_num = message_number
+      return nil unless (mt = MessageType[msg_num])
       # return [[value, df], ...]
-      decode(mt[:bits]).zip(mt[:op]).collect{|v, op|
-        op ? op.call(v) : v
-      }.zip(mt[:df])
+      values, df_list = [[], []]
+      add_proc = proc{|target, offset|
+        values += decode(target[:bits], offset).zip(target[:op]).collect{|v, op|
+          op ? op.call(v) : v
+        }
+        df_list += target[:df]
+      }
+      add_proc.call(mt)
+      case msg_num
+      when 1077
+        nsat, nsig = [-2, -1].collect{|i| values[i].size}
+        offset = 24 + mt[:bits_total]
+        df396 = DataFrame.generate_prop([[396, values[-2], values[-1]]])
+        add_proc.call(df396, offset)
+        ncell = values[-1].size
+        offset += df396[:bits_total]
+        msm7_sat = DataFrame.generate_prop(
+            ([[397, [:uint, 4], 398, 399]] * nsat).transpose.flatten(1))
+        add_proc.call(msm7_sat, offset)
+        offset += msm7_sat[:bits_total]
+        msm7_sig = DataFrame.generate_prop(
+            ([[405, 406, 407, 420, 408, 404]] * ncell).transpose.flatten(1))
+        add_proc.call(msm7_sig, offset)
+      end
+      values.zip(df_list)
     end
   end
   def read_packet
