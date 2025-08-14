31689e23e376276d6bca50510e3d42c81314407e
fenrir <fenrir.naru@gmail.com>
2025-08-14 10:39:45 +0900

    Add "-O3" to cflags for shared library build

--- ext/gps_pvt/extconf.rb
+++ ext/gps_pvt/extconf.rb
@@ -3,9 +3,14 @@ proc{|ninja_tool_dir|
   dir_config('ninja_tool', ninja_tool_dir, ninja_tool_dir)
 }.call(File::absolute_path(File::join(
     File::dirname(__FILE__), '..', 'ninja-scan-light', 'tool')))
-cflags = " -Wall"
-$CFLAGS += cflags
-$CPPFLAGS += cflags
+cflags = " -Wall -I../../.. -O3" # -march=native
+RE_optflags = /(?<=^|\s)-O(?:[0-3sgz]|fast)?/
+if RE_optflags =~ cflags then
+  $CFLAGS.gsub!(RE_optflags, '')
+  $CXXFLAGS.gsub!(RE_optflags, '')
+end
+$CFLAGS << cflags
+$CXXFLAGS << cflags
 $LOCAL_LIBS += " -lstdc++ "
 
 IO_TARGETS = [
